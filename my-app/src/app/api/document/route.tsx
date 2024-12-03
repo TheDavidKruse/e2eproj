e9080d48ad525645089b67013251f192d701689e
David Kruse <thedavidkruse@gmail.com>
2024-12-03 10:56:56 -0700

    mongo

--- my-app/src/app/api/document/route.tsx
+++ my-app/src/app/api/document/route.tsx
@@ -1,16 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import { REQUEST_METHODS } from '../helpers';
+import client from '../../../lib/mongo';
 
 type ResponseData = {
   message: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  if (req.method === REQUEST_METHODS.POST) {
+export async function GET(res: NextApiResponse<ResponseData>) {
+  // Replace the uri string with your MongoDB deployment's connection string.
+  try {
+    // Get the database and collection on which to run the operation
+    const database = client.db('local');
+    const movies = database.collection('Photos');
+    // Query for a movie that has the title 'The Room'
+    const query = { albumId: 1 };
+    // Execute query
+    const movie = await movies.findOne(query);
+    // Print the document returned by findOne()
+    console.log(movie);
+    return Response.json({ data: movie }, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return Response.json({ msg: 'calll failed' }, { status: 501 });
+  } finally {
+    await client.close();
   }
-  res.status(200).json({ message: 'ok' });
 }
--- my-app/src/lib/mongo.ts
+++ my-app/src/lib/mongo.ts
@@ -1,25 +1,30 @@
 import { MongoClient } from 'mongodb';
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'myProject';
-
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
-
-  // the following code examples can be pasted here...
-
-  return 'done.';
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient;
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  client = globalWithMongo._mongoClient;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+}
+
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
+export default client;
